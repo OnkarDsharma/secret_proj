cfdd0bad0e38a6a5a6773561b34da63e12d1fd0b
OnkarDsharma <onkareshwarsharma7om@gmail.com>
2026-08-16 18:03:29 +0530

    Insert interval

--- Inseer_Interval.cpp
+++ Inseer_Interval.cpp
@@ -1,1 +1,24 @@
-#include<stdio.h>
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> res;
+        int Aptr=0;
+        int Bptr=0;
+        int sizeA= firstList.size();
+        int sizeB= secondList.size();
+        vector<int> temp(2);
+
+        while(Aptr<sizeA && Bptr<sizeB){
+            if(firstList[Aptr][1] >= secondList[Bptr][0] && secondList[Bptr][1]>= firstList[Aptr][0]){
+                temp[0]= max(firstList[Aptr][0], secondList[Bptr][0]);
+                temp[1]= min(firstList[Aptr][1], secondList[Bptr][1]);
+                res.push_back(temp);
+            }
+            if(firstList[Aptr][1] > secondList[Bptr][1]){
+                Bptr++;
+            }
+            else Aptr++;
+        }
+        return res;
+    }
+};
